4c94f3fb7f95723eb0ec3ae8c98c08a9a2ca0827
Sami <melahi@uchicago.edu>
2021-04-12 21:19:26 -0700

    text input implemented, need to get rid of flags bc in canonical mode

--- text_editor/kilo.c
+++ text_editor/kilo.c
@@ -1,3 +1,10 @@
+#include <unistd.h>
+
 int main() {
+    char c;
+    // comes from unistd.h we're asking to read one byte
+    // from the standard input into the variable c, loop
+    // until there are no more bytes left to read
+    while (read(STDIN_FILENO, &c, 1) == 1);
     return 0;
 }
